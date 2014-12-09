fe33179441ec28469f7d1fb8bf3517bd93e61f6b
Muhammed Thanish <mnmtanish@gmail.com>
2014-12-09 17:56:37 +0530

    Add route middleware feature

--- client/flow-route.js
+++ client/flow-route.js
@@ -2,12 +2,21 @@ FlowRoute = function (path, options) {
   this.path = path;
   this.render = options.render || Function.prototype;
   this.subscriptions = options.subscriptions || Function.prototype;
+  this._middleware = [];
   this._states = new ReactiveDict;
   this._clientRouter.route(path, options);
 };
 
 
 FlowRoute.prototype._clientRouter = ClientRouter;
+
+
+FlowRoute.prototype.middleware = function (middleware) {
+  this._middleware.push(middleware);
+  this._clientRouter.middleware(middleware, {
+    path: this.path;
+  });
+};
 
 
 FlowRoute.prototype.setState = function (name, value, options) {
--- server/flow-route.js
+++ server/flow-route.js
@@ -1,3 +1,11 @@
 FlowRoute = function (path, options) {
-  // body...
+  this.path = path;
+  this.render = options.render || Function.prototype;
+  this.subscriptions = options.subscriptions || Function.prototype;
+  this._middleware = [];
+  this._states = new ReactiveDict;
+};
+
+FlowRoute.prototype.middleware = function (middleware) {
+  this._middleware.push(middleware);
 };
