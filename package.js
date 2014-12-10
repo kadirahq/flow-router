004079cb9732c0f32308bbe56857cd4194086e03
Muhammed Thanish <mnmtanish@gmail.com>
2014-12-10 13:04:12 +0530

    Reboot

--- package.js
+++ package.js
@@ -7,17 +7,10 @@ Package.describe({
 
 Package.onUse(function(api) {
   api.versionsFrom('1.0');
-  api.use('underscore');
-  api.use('tracker');
-  api.addFiles('client/client-router.js', 'client');
-  api.addFiles('common/flow-route.js');
-  api.addFiles('common/flow-router.js');
   api.export('FlowRouter');
 });
 
 Package.onTest(function(api) {
   api.use('tinytest');
   api.use('meteorhacks:flow-router');
-  api.addFiles('tests/flow-route.test.js');
-  api.addFiles('tests/flow-router.test.js');
 });
