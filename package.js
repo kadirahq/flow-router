a0af5dc11b1b22c6351e37e6919fa9e2195a2195
Arunoda Susiripala <arunoda.susiripala@gmail.com>
2015-01-01 04:15:49 +0530

    version bumped

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
   name: 'meteorhacks:flow-router',
   summary: 'Router for Flow Architecture',
-  version: '0.0.3',
+  version: '0.0.4',
   git: 'https://github.com/meteorhacks/flow-router.git'
 });
 
