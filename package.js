91a595ee1497a4688095dbc4fd282fd0a65aca7f
Arunoda Susiripala <arunoda.susiripala@gmail.com>
2015-04-17 11:54:46 +0530

    Bump version 1.3.0

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
   name: 'meteorhacks:flow-router',
   summary: 'Carefully Designed Client Side Router for Meteor',
-  version: '1.2.0',
+  version: '1.3.0',
   git: 'https://github.com/meteorhacks/flow-router.git'
 });
 
