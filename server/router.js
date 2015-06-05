0797cbc88da694e25e370823d484d6b71ebb6754
Delgermurun <pdelgermurun@gmail.com>
2015-06-05 15:21:26 +0900

    Implement "path" method on server. Fix #135.

--- server/router.js
+++ server/router.js
@@ -1,16 +1,53 @@
+var Qs = Npm.require('qs');
+
 Router = function () {
   this._routesMap = {};
+  this._routeNamesMap = {};
   this.subscriptions = Function.prototype;
 };
 
 
 Router.prototype.route = function(path, options) {
   this._routesMap[path] = new Route(this, path, options);
+
+  if (options.name) {
+    this._routeNamesMap[options.name] = path;
+  }
+
   return this._routesMap[path];
 };
 
 Router.prototype.group = function(options) {
   return new Group(this, options);
+};
+
+Router.prototype.path = function(pathDef, fields, queryParams) {
+  if (this._routeNamesMap[pathDef]) {
+    pathDef = this._routeNamesMap[pathDef];
+  }
+
+  if (this._routesMap[pathDef]) {
+    pathDef = this._routesMap[pathDef].path;
+  }
+
+  fields = fields || {};
+  var regExp = /(:[\w\(\)\\\+\*\.\?]+)+/g;
+  var path = pathDef.replace(regExp, function(key) {
+    var firstRegexpChar = key.indexOf("(");
+    // get the content behind : and (\\d+/)
+    key = key.substring(1, (firstRegexpChar > 0)? firstRegexpChar: undefined);
+    // remove +?*
+    key = key.replace(/[\+\*\?]+/g, "");
+
+    return fields[key] || "";
+  });
+
+  var strQueryParams = Qs.stringify(queryParams || {});
+  if(strQueryParams) {
+    path += "?" + strQueryParams;
+  }
+
+  return path;
 };
 
 
