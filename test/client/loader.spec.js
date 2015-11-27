ccedf3894cc5193c87e1e6096930cdff9d34794b
Elie Steinbock <eliesteinbock@gmail.com>
2015-11-27 11:18:35 +0200

    cleaned up router a bit and moved some tests to common

--- test/client/loader.spec.js
+++ test/client/loader.spec.js
@@ -1,17 +1,4 @@
-Router = FlowRouter.Router;
-
-
 Tinytest.add('Client - import page.js', function (test) {
-  test.isTrue(!!Router.prototype._page);
+  test.isTrue(!!FlowRouter._page);
   test.isFalse(!!window.page);
-});
-
-
-Tinytest.add('Client - import query.js', function (test) {
-  test.isTrue(!!Router.prototype._qs);
-});
-
-
-Tinytest.add('Client - create FlowRouter', function (test) {
-  test.isTrue(!!FlowRouter);
 });
