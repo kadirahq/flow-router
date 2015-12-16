8d72167c1e922e96ba01f9e7913a5b25e7303a29
TJ <tjmonsi@gmail.com>
2015-12-16 20:49:24 +0800

    Reverted group test to just work only on client

--- package.js
+++ package.js
@@ -40,7 +40,7 @@ Package.onTest(function(api) {
   api.addFiles('test/client/loader.spec.js', 'client');
   api.addFiles('test/client/route.reactivity.spec.js', 'client');
   api.addFiles('test/client/router.core.spec.js', 'client');
-  api.addFiles('test/client/router.reactivity.spec.js', 'client');  
+  api.addFiles('test/client/router.reactivity.spec.js', 'client');
   api.addFiles('test/client/trigger.spec.js', 'client');
   api.addFiles('test/client/triggers.js', 'client');
 
