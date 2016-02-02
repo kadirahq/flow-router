52075a39167fea95cf2f47b5b79ba8fe65476046
PEM-- <pemarchandet@gmail.com>
2016-02-02 10:52:22 +0100

    Circumvent Babel issue in Meteor 6138

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
   name: 'kadira:flow-router-ssr',
   summary: 'Same as kadira:flow-router, but with SSR support',
-  version: '3.9.0',
+  version: '3.9.1',
   git: 'https://github.com/kadirahq/flow-router.git'
 });
 
